ab8aad196b5d8aa71b627cda2522ea24dfc63874
Tobias Länge <tobias.laenge@kit.edu>
2025-02-03 16:56:28 +0100

    Fix crash in TutorialActivity if more than 3 slides are used

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/TutorialActivity.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/TutorialActivity.java
@@ -32,6 +32,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -75,34 +76,32 @@ public class TutorialActivity extends AppCompatActivity {
         }
 
         // Making notification bar transparent
-        if(Build.VERSION.SDK_INT >=21)
-
-        {
+        if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         setContentView(R.layout.activity_tutorial);
 
-        viewPager =(ViewPager)
+        viewPager = (ViewPager)
 
                 findViewById(R.id.viewPager);
 
-        dotsLayout =(LinearLayout)
+        dotsLayout = (LinearLayout)
 
                 findViewById(R.id.dotsLayout);
 
-        btnSkip =(Button)
+        btnSkip = (Button)
 
                 findViewById(R.id.btSkip);
 
-        btnNext =(Button)
+        btnNext = (Button)
 
                 findViewById(R.id.btNext);
 
 
         // layouts of all welcome sliders
         // add few more layouts if you want
-        layouts =new int[]
+        layouts = new int[]
 
                 {
                         R.layout.tutorial_slide1,
@@ -117,26 +116,22 @@ public class TutorialActivity extends AppCompatActivity {
         // making notification bar transparent
         changeStatusBarColor();
 
-        myViewPagerAdapter =new
+        myViewPagerAdapter = new
 
                 MyViewPagerAdapter();
         viewPager.setAdapter(myViewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
-        btnSkip.setOnClickListener(new View.OnClickListener()
-
-        {
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 launchHomeScreen();
             }
         });
 
-        btnNext.setOnClickListener(new View.OnClickListener()
-
-        {
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
+            public void onClick(View v) {
                 // checking for last page
                 // if last page home screen will be launched
                 int current = getItem(+1);
@@ -153,20 +148,20 @@ public class TutorialActivity extends AppCompatActivity {
     private void addBottomDots(int currentPage) {
         dots = new TextView[layouts.length];
 
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+        @ColorInt int colorActive = getResources().getColor(R.color.dot_light_screen, getTheme());
+        @ColorInt int colorInactive = getResources().getColor(R.color.dot_dark_screen, getTheme());
 
         dotsLayout.removeAllViews();
         for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226;"));
             dots[i].setTextSize(35);
-            dots[i].setTextColor(colorsInactive[currentPage]);
+            dots[i].setTextColor(colorInactive);
             dotsLayout.addView(dots[i]);
         }
 
         if (dots.length > 0)
-            dots[currentPage].setTextColor(colorsActive[currentPage]);
+            dots[currentPage].setTextColor(colorActive);
     }
 
     private int getItem(int i) {
