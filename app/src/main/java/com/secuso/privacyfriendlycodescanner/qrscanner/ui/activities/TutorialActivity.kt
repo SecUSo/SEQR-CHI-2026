0259a3fde25d54474ed5de75afbf4abac2da00ee
Tobias Länge <tobias.laenge@kit.edu>
2025-04-15 11:49:46 +0200

    Convert `TutorialActivity.java` to `TutorialActivity.kt`

---
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/TutorialActivity.kt
@@ -0,0 +1,226 @@
+/*
+ This file is part of Privacy Friendly App Example.
+
+ Privacy Friendly App Example is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+
+ Privacy Friendly App Example is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly App Example. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import com.secuso.privacyfriendlycodescanner.qrscanner.R
+import com.secuso.privacyfriendlycodescanner.qrscanner.helpers.PrefManager
+
+/**
+ * Class structure taken from tutorial at http://www.androidhive.info/2016/05/android-build-intro-slider-app/
+ *
+ * @author Karola Marky
+ * @version 20161214
+ */
+class TutorialActivity : AppCompatActivity() {
+    private var viewPager: ViewPager? = null
+    private var myViewPagerAdapter: MyViewPagerAdapter? = null
+    private var dotsLayout: LinearLayout? = null
+    private lateinit var dots: Array<TextView?>
+    private lateinit var layouts: IntArray
+    private var btnSkip: Button? = null
+    private var btnNext: Button? = null
+    private var prefManager: PrefManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Checking for first time launch - before calling setContentView()
+        prefManager = PrefManager(this)
+        val i = intent
+
+
+        if (!prefManager!!.isFirstTimeLaunch && (i == null || ACTION_SHOW_ANYWAYS != i.action)) {
+            launchHomeScreen()
+            return
+        }
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+
+        setContentView(R.layout.activity_tutorial)
+
+        viewPager = findViewById<View>(R.id.viewPager) as ViewPager
+
+        dotsLayout = findViewById<View>(R.id.dotsLayout) as LinearLayout
+
+        btnSkip = findViewById<View>(R.id.btSkip) as Button
+
+        btnNext = findViewById<View>(R.id.btNext) as Button
+
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = intArrayOf(
+            R.layout.tutorial_slide1,
+            R.layout.tutorial_slide2,
+            R.layout.tutorial_slide3,
+            R.layout.url_tutorial_slide1,
+            R.layout.url_tutorial_slide2,
+            R.layout.url_tutorial_slide3,
+            R.layout.url_tutorial_slide4,
+            R.layout.url_tutorial_slide5,
+            R.layout.url_tutorial_slide6,
+            R.layout.url_tutorial_slide7,
+            R.layout.url_tutorial_slide8,
+            R.layout.url_tutorial_slide9,
+        )
+
+        // adding bottom dots
+        addBottomDots(0)
+
+        // making notification bar transparent
+        changeStatusBarColor()
+
+        myViewPagerAdapter = MyViewPagerAdapter()
+        viewPager!!.adapter = myViewPagerAdapter
+        viewPager!!.addOnPageChangeListener(viewPagerPageChangeListener)
+
+        btnSkip!!.setOnClickListener { launchHomeScreen() }
+
+        btnNext!!.setOnClickListener {
+            // checking for last page
+            // if last page home screen will be launched
+            val current = getItem(+1)
+            if (current < layouts.size) {
+                // move to next screen
+                viewPager!!.currentItem = current
+            } else {
+                launchHomeScreen()
+            }
+        }
+    }
+
+    private fun addBottomDots(currentPage: Int) {
+        dots = arrayOfNulls(layouts.size)
+
+        @ColorInt val colorActive = resources.getColor(R.color.dot_light_screen, theme)
+        @ColorInt val colorInactive = resources.getColor(R.color.dot_dark_screen, theme)
+
+        dotsLayout!!.removeAllViews()
+        for (i in dots.indices) {
+            dots[i] = TextView(this)
+            dots[i]!!.text = Html.fromHtml("&#8226;")
+            dots[i]!!.textSize = 35f
+            dots[i]!!.setTextColor(colorInactive)
+            dotsLayout!!.addView(dots[i])
+        }
+
+        if (dots.size > 0) dots[currentPage]!!.setTextColor(colorActive)
+    }
+
+    private fun getItem(i: Int): Int {
+        return viewPager!!.currentItem + i
+    }
+
+    private fun launchHomeScreen() {
+        prefManager!!.isFirstTimeLaunch = false
+        val intent = Intent(this, ScannerActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
+    //  viewpager change listener
+    var viewPagerPageChangeListener: OnPageChangeListener = object : OnPageChangeListener {
+        override fun onPageSelected(position: Int) {
+            addBottomDots(position)
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.size - 1) {
+                // last page. make button text to GOT IT
+                btnNext!!.setText(R.string.okay)
+                btnSkip!!.visibility = View.GONE
+            } else {
+                // still pages are left
+                btnNext!!.setText(R.string.next)
+                btnSkip!!.visibility = View.VISIBLE
+            }
+        }
+
+        override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {
+        }
+
+        override fun onPageScrollStateChanged(arg0: Int) {
+        }
+    }
+
+    /**
+     * Making notification bar transparent
+     */
+    private fun changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    /**
+     * View pager adapter
+     */
+    inner class MyViewPagerAdapter : PagerAdapter() {
+        private var layoutInflater: LayoutInflater? = null
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            layoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+            val view = layoutInflater!!.inflate(layouts[position], container, false)
+            container.addView(view)
+
+            return view
+        }
+
+        override fun getCount(): Int {
+            return layouts.size
+        }
+
+        override fun isViewFromObject(view: View, obj: Any): Boolean {
+            return view === obj
+        }
+
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            val view = `object` as View
+            container.removeView(view)
+        }
+    }
+
+    companion object {
+        private val TAG: String = TutorialActivity::class.java.simpleName
+        @JvmField
+        val ACTION_SHOW_ANYWAYS: String = TAG + ".ACTION_SHOW_ANYWAYS"
+    }
+}
